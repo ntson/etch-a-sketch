70cad6a576d5a7694b1c263ed9f701bc854432ec
Nguyen Son <ngso.ts@gmail.com>
2021-12-08 10:39:28 +0700

    styling and eraser button

--- script.js
+++ script.js
@@ -1,12 +1,28 @@
 // Create grid
 const grid = document.getElementById('grid');
 
+let mode = '';
+
+const clearButton = document.getElementById('clear-button');
+clearButton.addEventListener('click', function () {
+  mode = 'clear';
+});
+
+const beginButton = document.getElementById('begin-button');
+beginButton.addEventListener('click', function () {
+  mode = 'draw';
+});
+
 // Fill up the grid with squares
 for (let i = 0; i < 16 * 16; i++) {
   const square = document.createElement('div');
   square.classList.add('square');
   grid.appendChild(square);
   square.addEventListener('mouseenter', function () {
-    square.classList.add('filled');
+    if (mode === 'draw') {
+      square.classList.add('filled');
+    } else if (mode === 'clear') {
+      square.classList.remove('filled');
+    }
   });
 }
